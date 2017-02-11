4ab5a583558584a7e11ab062801bef974fb64964
Marek Bozych <m.bozych@gmail.com>
2017-02-11 21:44:44 +0100

    CHANGE : get posts and groups from group-details component

--- client/src/app/components/group-details/group-details.component.js
+++ client/src/app/components/group-details/group-details.component.js
@@ -4,12 +4,32 @@ import template from './group-details.html';
 //import './group-details.scss';
 
 export class GroupDetailsController {
-  constructor(GroupService) {
-    GroupService.all().then( (res) => this.groups = res.data );
+  constructor(GroupService, $stateParams) {
+    this._GroupService = GroupService;
+    this.page = 1;
+    this.group = {
+      id: $stateParams.groupId,
+      posts: []
+    };
+  }
+
+  $onInit() {
+    // Get posts
+    this._GroupService
+      .posts( { groupId: this.group.id, page: this.page } )
+      .then( (res) => {
+        this.group.posts = res.data.posts;
+        this.group.name = res.data.group.name;
+      })
+
+    // Get groups
+    this._GroupService
+      .all()
+      .then( (res) => this.groups = res.data );
   }
 }
 
-GroupDetailsController.$inject = ['GroupService'];
+GroupDetailsController.$inject = ['GroupService', '$stateParams'];
 
 let groupDetailsComponent = {
   bindings: {},
--- client/src/app/components/group-posts/group-posts.component.js
+++ client/src/app/components/group-posts/group-posts.component.js
@@ -5,28 +5,14 @@ import './group-posts.scss';
 
 export class GroupPostsController {
   /*@ngInject*/
-  constructor(GroupService, $stateParams) {
-    this._GroupService = GroupService;
-    this.page = 1;
-    this.group = {
-      id: $stateParams.groupId,
-      posts: []
-    };
-  }
-
-  $onInit() {
-    this._GroupService
-      .posts( { groupId: this.group.id, page: this.page } )
-      .then( (res) => {
-        this.group.posts = res.data.posts;
-        this.group.name = res.data.group.name;
-      })
-  }
 }
 
 let groupPostsComponent = {
   template: template,
-  controller: GroupPostsController
+  controller: GroupPostsController,
+  bindings: {
+    posts: '<'
+  }
 }
 
 export default groupPostsComponent;
